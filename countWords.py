cadc8dfcb92eeb19925d4346af216643e3f798d0
AndrewDowns <adowns1991@outlook.com>
2017-07-07 17:08:51 +0100

    Update countWords.py

--- countWords.py
+++ countWords.py
@@ -2,9 +2,12 @@ import os
 import operator
 
 def listdir_fullpath(d):
+    #Function to create the full path of the directory or file given
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 def getWordFiles(rootDir):
+    #Function to retrieve the txt files from the given directory and any further subdirectories
+    #This function will return a list with the full path file names of each txt file
     dirList = [rootDir]
     fileList = []
     while(len(dirList)!=0):
@@ -16,6 +19,8 @@ def getWordFiles(rootDir):
     return fileList
 
 def countWords(fileList):
+    #Function to count occurrences of words in a series of txt files provided in a list
+    #This function will return a dictionary where the structure is {"word", number of occurrences}
     words = {}
     for i in fileList:
         wordFile = open(i, "r+")
@@ -27,9 +32,14 @@ def countWords(fileList):
     return words
 
 def sortWords(wordList):
+    #Function to sort a dictionary of words and their occurrences
+    #The function will sort the keys by their value and return a list of tuples
+    # The sorted list will be in Descending Order
     return sorted(wordList.items(), key=operator.itemgetter(1), reverse=True)
 
 def displayTop10(wordList):
+    #Function to display the top 10 words and the number of occurrences
+    #If less than 10 words exist in all of the txt files then the top x number of words will be displayed with x being the number of words that exist
     displayCount = 10
     if (len(wordList)<10):
         displayCount = len(wordList)
@@ -37,6 +47,8 @@ def displayTop10(wordList):
     for i in range(displayCount):
         print( str((i+1))+ ". "+ wordList[i][0] + "("+str(wordList[i][1])+" times)")
 
+#Ask the user for the path of the root directory
+#Keep asking until they provide a valid path
 pathname = ""
 while (os.path.isdir(pathname)==False):
     pathname = input('Enter a valid directory path: ')
